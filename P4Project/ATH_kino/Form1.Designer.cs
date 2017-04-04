afaae899a6220c83bb123e890c780c32fb597137
MJ <MJ@BOGUSIA-PC>
2017-04-04 19:20:28 +0200

    commit

---
+++ P4Project/ATH_kino/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace ATH_kino
+{
+    partial class Form1
+{
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBoxFilmsList = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.labelDirector = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelNameSolid = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(6, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 300);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBoxFilmsList
+            // 
+            this.comboBoxFilmsList.FormattingEnabled = true;
+            this.comboBoxFilmsList.Location = new System.Drawing.Point(212, 19);
+            this.comboBoxFilmsList.Name = "comboBoxFilmsList";
+            this.comboBoxFilmsList.Size = new System.Drawing.Size(200, 21);
+            this.comboBoxFilmsList.TabIndex = 3;
+            this.comboBoxFilmsList.Text = "Wybierz film...";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 362);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.comboBoxFilmsList);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(10, 10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(677, 391);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Lista Filmów";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.labelDirector);
+            this.groupBox2.Controls.Add(this.labelName);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.labelNameSolid);
+            this.groupBox2.Location = new System.Drawing.Point(212, 46);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            // 
+            // labelDirector
+            // 
+            this.labelDirector.AutoSize = true;
+            this.labelDirector.Location = new System.Drawing.Point(79, 31);
+            this.labelDirector.Name = "labelDirector";
+            this.labelDirector.Size = new System.Drawing.Size(34, 13);
+            this.labelDirector.TabIndex = 5;
+            this.labelDirector.Text = "(brak)";
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(79, 13);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(34, 13);
+            this.labelName.TabIndex = 5;
+            this.labelName.Text = "(brak)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Reżyser:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Reżyser:";
+            // 
+            // labelNameSolid
+            // 
+            this.labelNameSolid.AutoSize = true;
+            this.labelNameSolid.Location = new System.Drawing.Point(6, 13);
+            this.labelNameSolid.Name = "labelNameSolid";
+            this.labelNameSolid.Size = new System.Drawing.Size(43, 13);
+            this.labelNameSolid.TabIndex = 5;
+            this.labelNameSolid.Text = "Nazwa:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(698, 411);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.PictureBox pictureBox1;
+    private System.Windows.Forms.ComboBox comboBoxFilmsList;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.GroupBox groupBox1;
+    private System.Windows.Forms.GroupBox groupBox2;
+    private System.Windows.Forms.Label labelNameSolid;
+    private System.Windows.Forms.Label labelName;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label labelDirector;
+    private System.Windows.Forms.Label label2;
+}
+}
+
---
+++ P4Project/ATH_kino/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity.Infrastructure;
+
+namespace ATH_kino
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void comboBoxFilmsList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            using (var ctx = new ATH_kinoEntities())
+            {
+                string selected = comboBoxFilmsList.SelectedItem.ToString();
+                labelName.Text = selected;
+               // labelDirector.Text =
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            using (var ctx = new ATH_kinoEntities())
+            {
+                var data = ctx.Film;
+                comboBoxFilmsList.Items.Add("(brak)");
+                foreach (var item in data)
+                {
+                    comboBoxFilmsList.Items.Add($"{item.Nazwa}");
+                }
+            }
+        }
+    }
+}
+
